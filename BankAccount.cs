9a06c5ae1d15cade13a0c39faae31e0cb384c2a5
Abdiaziiz Ali mohamed Arrale <abdiaziizarrale@gmail.com>
2022-09-08 14:42:38 +0300

    adding Deposite

--- BankAccount.cs
+++ BankAccount.cs
@@ -4,11 +4,15 @@ class BankAccount
 {
      public int AccountNumber { get;  }
      public string Owner { get; set; }
-     public decimal Balance { get;  }
+     public decimal Balance { get; private set; }
      public BankAccount(int accountnum, string owner,int initialnalance)
      {
         AccountNumber=accountnum;
         Owner=owner;
         Balance= initialnalance;
+     }
+     public void Deposite(Decimal ammount)
+     {
+        Balance= Balance+ammount;
      }
 }
--- Program.cs
+++ Program.cs
@@ -6,4 +6,6 @@
 
 var account1= new BankAccount(1,"abdiaziiz",10);
 // account1.Balance=200;
+account1.Deposite(100);
+
 System.Console.WriteLine("owner is :"+account1.Owner +"balance:"+account1.Balance.ToString("c"));

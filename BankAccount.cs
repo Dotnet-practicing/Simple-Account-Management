71911d953f60974272defaed0ffc9ce7d4422f9d
Abdiaziiz Ali mohamed Arrale <abdiaziizarrale@gmail.com>
2022-09-08 15:11:53 +0300

    adding whithdraw

--- BankAccount.cs
+++ BankAccount.cs
@@ -2,17 +2,26 @@
 
 class BankAccount
 {
-     public int AccountNumber { get;  }
-     public string Owner { get; set; }
-     public decimal Balance { get; private set; }
-     public BankAccount(int accountnum, string owner,int initialnalance)
-     {
-        AccountNumber=accountnum;
-        Owner=owner;
-        Balance= initialnalance;
-     }
-     public void Deposite(Decimal ammount)
-     {
-        Balance= Balance+ammount;
-     }
+    public int AccountNumber { get; }
+    public string Owner { get; set; }
+    public decimal Balance { get; private set; }
+    public BankAccount(int accountnum, string owner, int initialnalance)
+    {
+        AccountNumber = accountnum;
+        Owner = owner;
+        Balance = initialnalance;
+    }
+    public void Deposit(Decimal ammount)
+    {
+        Balance = Balance + ammount;
+    }
+    public void Whithdraw(decimal ammount)
+    {
+        if (ammount > Balance)
+        {
+            System.Console.WriteLine("ma haysatid lacag kugu filan");
+            return;
+        }
+        Balance = Balance - ammount;
+    }
 }
--- Program.cs
+++ Program.cs
@@ -2,10 +2,10 @@
 //account Bank: Owner,Balance,AccountNumber
 //deposite
 //Whithdraw
+// ToString("c")); this for dollar sing
 
-
-var account1= new BankAccount(1,"abdiaziiz",10);
+var account1= new BankAccount(1,"abdiaziiz",40);
 // account1.Balance=200;
-account1.Deposite(100);
-
+// account1.Deposit(100);
+account1.Whithdraw(50);
 System.Console.WriteLine("owner is :"+account1.Owner +"balance:"+account1.Balance.ToString("c"));
